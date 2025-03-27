cce1ed78ffd6520974c1914d60304199e5d6b642
Sanka-Vamsi-Akshay <sankavamsiakshay@gmail.com>
2025-03-27 22:39:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,29 @@ import joblib
 model = joblib.load("loan.joblib")
 st.title("Loan Prediction App")
 st.markdown("### Input Data")
-gender = st.number_input("Gender (Male : 0, Female : 1)", min_value = 0, max_value = 1, step = 1)
-married = st.number_input("Marital Status (Unmarried : 0, Married : 1)", min_value = 0, max_value = 1, step = 1)
-income = st.number_input("Income", min_value = 1000, step = 1)
-loan_amount = st.number_input("Loan Amount", min_value = 1000, step = 1)
+gender = st.radio("Select Gender", ["Male", "Female"])
+gender = 0 if gender == "Male" else 1 
+married = st.radio("Marital Status", ["Unmarried", "Married"])
+married = 0 if married == "Unmarried" else 1
+income = st.number_input("Income", min_value = 1000, step = 1000)
+loan_amount = st.number_input("Loan Amount", min_value = 1000, step = 1000)
 
 if st.button("Loan Status"):
     prediction = model.predict([[gender, married, income, loan_amount]])
     if prediction == 'Y':
-        st.success("Loan is Approved")
+        st.success("✅ Loan is Approved")
     else:
-        st.error("Loan is Rejected")
+        st.error("❌ Loan is Rejected")
+
+st.markdown("---")
+
+st.markdown("### Loan Prediction App")
+st.markdown("*Built using Decision Tree Machine Learning Algorithm*")
+
+st.write("")
+
+st.markdown("> Initial concept by **Kiran Kalla**, refined and developed further by **Sanka Vamsi Akshay**.")
+
+st.write("---")
+
+st.markdown("⚠️ *Disclaimer: This is a basic implementation for learning purposes. Predictions may not be accurate.*")
