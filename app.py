9604720b1913c134d2560d8ac01dad9f3cb2e825
Sanka-Vamsi-Akshay <sankavamsiakshay@gmail.com>
2025-03-27 11:23:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Loan Prediction App")
 st.markdown("### Input Data")
 gender = st.number_input("Gender (Male : 0, Female : 1)", min_value = 0, max_value = 1, step = 1)
 married = st.number_input("Marital Status (Unmarried : 0, Married : 1)", min_value = 0, max_value = 1, step = 1)
-income = st.number_input("Income",step = 1)
+income = st.number_input("Income", min_value = 1000, step = 1)
 loan_amount = st.number_input("Loan Amount", min_value = 1000, step = 1)
 
 if st.button("Loan Status"):
